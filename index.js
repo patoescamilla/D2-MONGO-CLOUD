45c70c5238399b726de612e0d99330fb5156bfce
Gerardo Patricio Escamilla Carillo <patoescamillac@gmail.com>
2026-02-12 14:59:24 -0600

    users

--- index.js
+++ index.js
@@ -14,8 +14,4 @@ app.get('/', (req, res) => {
     res.send('Hello MongoDB!');
 });
 
-// Start the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;

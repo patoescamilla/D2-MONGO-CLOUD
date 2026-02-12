09c98db8f240615de596ff9d7e3953cf695f3b72
Gerardo Patricio Escamilla Carillo <patoescamillac@gmail.com>
2026-02-12 15:08:34 -0600

    Rename api function

--- api/test.js
+++ api/test.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const connectDB = require('../db');
-require('dotenv').config();
+
 const app = express();
 
-// Connect to MongoDB
 connectDB();
 
-// Middleware to parse JSON
-app.use(express.json());
-
-// Define a simple route
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
     res.send('Hello MongoDB!');
 });
 

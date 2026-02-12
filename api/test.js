0b44562ff81b745cb3ec77eb9bdcbcd6bb556839
Gerardo Patricio Escamilla Carillo <patoescamillac@gmail.com>
2026-02-12 15:19:53 -0600

    Add Express to serverless function

--- api/test.js
+++ api/test.js
@@ -1,3 +1,17 @@
+const express = require('express');
+const connectDB = require('../db');
+
+const app = express();
+
+connectDB();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.status(200).send("Hello MongoDB!");
+});
+
+// 👇 export correcto para Vercel
 module.exports = (req, res) => {
-  res.status(200).send("Server is working");
+  return app(req, res);
 };

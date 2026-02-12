510365b57ebcd948e53958178bf889dc42399d21
Gerardo Patricio Escamilla Carillo <patoescamillac@gmail.com>
2026-02-12 15:11:53 -0600

    Test raw serverless function

--- api/test.js
+++ api/test.js
@@ -1,17 +1,3 @@
-const express = require('express');
-const connectDB = require('../db');
-
-const app = express();
-
-connectDB();
-
-app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.status(200).send('Hello MongoDB!');
-});
-
-// 👇 ESTA LÍNEA ES CLAVE
 module.exports = (req, res) => {
-    return app(req, res);
+  res.status(200).send("Server is working");
 };
